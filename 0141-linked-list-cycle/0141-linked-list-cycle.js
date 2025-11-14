61bca40379acafde69aa50488418de02360d2124
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2025-11-14 13:02:55 +0530

    Time: 61 ms (36.91%), Space: 59.5 MB (57.28%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let curr = head
+    if(!curr || !curr.next) return false
+    let set = new Set()
+
+    while(curr){
+        if(set.has(curr)){
+            return true
+        }
+        else{
+            set.add(curr)
+        }
+        curr = curr.next
+    }
+    return false
+};

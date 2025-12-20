f9e50c46bf026b251871ffaf7a764381cea7b4f1
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2025-12-20 15:59:07 +0530

    Time: 0 ms (100%), Space: 57.1 MB (15.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    if(x==0 || x==1) return x
+    if(x < 2) return x
     let l = 2
     let r = Math.floor(x/2)
 
     while(l <= r){
         let mid = Math.floor((l + r)/2)
-        if(mid * mid == x){
+        // console.log(mid)
+        if(mid*mid === x) { 
             return mid
         }
-        else if(mid * mid > x){
-            r = mid-1
+
+        else if(mid*mid > x){
+            r = mid - 1
         }
         else{
             l = mid + 1
@@ -21,3 +23,9 @@ var mySqrt = function(x) {
     }
     return r
 };
+
+// l = 2
+// r = 4  mid = 3
+
+// l=2
+// r=2

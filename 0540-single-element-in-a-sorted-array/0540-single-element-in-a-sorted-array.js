7c4ffdea6fa9d96bd338be88a7d5716c71c92bbf
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2025-12-26 14:40:40 +0530

    Time: 0 ms (100%), Space: 57.5 MB (68.89%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -2,32 +2,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNonDuplicate = function(arr) {
+var singleNonDuplicate = function(nums) {
     let l = 0
-    let r = arr.length - 1
-    
-    while(l <= r) {
-        let mid = Math.floor((l + r)/2)
-        if(arr[mid-1] == arr[mid]){
-            let length = mid-l-1
-            if(length % 2 !== 0){
+    let r = nums.length - 1
+
+    while(l<=r){
+        let mid = Math.floor((l+r)/2)
+        if(nums[mid-1] == nums[mid]){
+            let len = mid-l-1
+            if(len%2 !== 0){
                 r = mid-2
             }
             else{
                 l = mid+1
             }
         }
-        else if(arr[mid] == arr[mid+1]){
-            let length = r-mid-1
-            if(length % 2 !== 0){
-                l = mid + 2
+        else if(nums[mid] == nums[mid+1]){
+            let len = r-mid-1
+            if(len%2 !== 0){
+                l = mid+2
             }
             else{
-                r = mid - 1
+                r = mid-1
             }
         }
         else{
-            return arr[mid]
+            return nums[mid]
         }
     }
 };

d410d99a913421ca227c797758ff5a5fa16366ec
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2025-12-26 23:10:13 +0530

    Time: 0 ms (100%), Space: 53.4 MB (89.48%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,15 +10,13 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let curr = head
-    let len = 0
-    while(curr){
-        len += 1
-        curr = curr.next
+    let f = head
+    let s = head
+
+    while(s && s.next){
+        f = f.next
+        s = s.next.next 
     }
-    let curr1 = head
-    for(let i=0;i<Math.floor(len/2);i++){
-        curr1 = curr1.next
-    }
-    return curr1
+
+    return f
 };

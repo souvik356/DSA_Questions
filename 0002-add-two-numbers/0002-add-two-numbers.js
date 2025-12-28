86ba54c0a3555a5c452c1c68aef6971757de4adf
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2025-12-29 01:57:45 +0530

    Time: 2 ms (84.64%), Space: 61.7 MB (50.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,18 +11,17 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let sentinelNode = new ListNode()
-    let curr = sentinelNode
-    let carry = 0
+    let sentNode = new ListNode()
+    let carry=0
+    let sentNodeStart = sentNode
     while(l1 || l2 || carry){
-        let sum = (l1 ? l1.val:0)+(l2 ? l2.val:0)+carry
+        let sum = (l1 ? l1.val:0) + (l2 ? l2.val:0) + carry
         carry = Math.floor(sum/10)
-        let newNode = new ListNode(sum % 10)
-        curr.next = newNode
-        curr = curr.next
-        l1 = l1 && l1.next
-        l2 = l2 && l2.next
+        let node = new ListNode(sum%10)
+        sentNode.next=node
+        sentNode=sentNode.next
+        if(l1) l1=l1.next
+        if(l2) l2=l2.next
     }
-
-    return sentinelNode.next
+    return sentNodeStart.next
 };

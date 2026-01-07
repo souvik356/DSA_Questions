40e9d83920b30333e2af03a3b0a358ceeb34e37a
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-01-07 15:56:55 +0530

    Time: 2 ms (38.16%), Space: 61 MB (5.2%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -3,15 +3,14 @@
  * @param {character} x
  * @return {number[]}
  */
-var findWordsContaining = function(words, x) { 
+var findWordsContaining = function(words, x) {
     let arr = []
-    for(let i = 0;i < words.length;i++){
-        for(let j=0;j<words[i].length;j++){
-            if(words[i][j] == x){
-                arr.push(i)
-                break
-            }
+    for(let i=0;i < words.length;i++){
+        let str = words[i]
+        let strArr = str.split('')
+        if(strArr.includes(x)){
+            arr.push(i)
         }
     }
     return arr
- };
+};

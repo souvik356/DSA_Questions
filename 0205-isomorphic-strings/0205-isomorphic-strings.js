d7e6757253c0db2ee4ea4186ebaafed9aeda6fac
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-01-07 17:05:36 +0530

    Time: 5 ms (80.22%), Space: 54.9 MB (81.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,19 +4,18 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    if(s.length !== t.length) return false
-    let mapST={}
-    let mapTS={}
+    let mapST = {}
+    let mapTS = {}
 
     for(let i=0;i<s.length;i++){
         if(!mapST[s[i]] && !mapTS[t[i]]){
-            mapST[s[i]]=t[i]
-            mapTS[t[i]]=s[i]
+            mapST[s[i]] = t[i]
+            mapTS[t[i]] = s[i]
         }
         else if(mapST[s[i]] !== t[i]){
             return false
         }
-        else if(mapTS[t[i]]!== s[i]){
+        else if(mapTS[t[i]] !== s[i]){
             return false
         }
     }

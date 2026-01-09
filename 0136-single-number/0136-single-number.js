060808f2138f50bcbf41ec1a1cac14277e3d7a37
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-01-09 17:47:29 +0530

    Time: 0 ms (100%), Space: 53.2 MB (99.85%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,15 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    for(let i = 0;i<nums.length;i++){
-        let count = 0
-        for(let j = 0;j<nums.length;j++){
-            if(nums[i] === nums[j]){
-                count += 1
-            }
-        }
-        if(count === 1){
-            return nums[i]
-        }
+    let sum = 0 
+    for(let i=0;i<nums.length;i++){
+        sum = sum ^ nums[i]
     }
+    return sum
 };

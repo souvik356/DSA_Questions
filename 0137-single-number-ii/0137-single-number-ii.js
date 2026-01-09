9651e6c9631d6608744428a855a0dbea8db8e6d7
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-01-09 18:19:30 +0530

    Time: 2 ms (86.17%), Space: 55.1 MB (87.38%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let map = {}
+    for(let i=0;i<nums.length;i++){
+        !map[nums[i]] ? map[nums[i]]=1 : ++map[nums[i]]
+    }
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]] == 1) return nums[i]
+    }
+};

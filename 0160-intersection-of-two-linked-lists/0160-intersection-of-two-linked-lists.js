30e754c2e9ccd368d784ca79fe7f66689afe00ee
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-03-03 18:14:18 +0530

    Time: 76 ms (20.22%), Space: 68.7 MB (34.85%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,37 +12,47 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let currA=headA
-    let currB=headB
-    let lengthA=0
-    let lengthB=0
+    let currA = headA
+    let currB = headB
+    let lengthA = 0
+    let lengthB = 0
+
     while(currA){
-        lengthA=lengthA+1
+        lengthA+=1
         currA=currA.next
     }
     while(currB){
-        lengthB=lengthB+1
+        lengthB+=1
         currB=currB.next
     }
-    // first as small second as large
-    if(lengthA>lengthB){
-        let temp=headA
-        headA=headB
-        headB=temp
-    }
-
-    for(let i=0;i<Math.abs(lengthA-lengthB);i++){
-        headB=headB.next
-    }
-    currA=headA
-    currB=headB
-
-    while(currA && currB){
-        if(currA=== currB){
-            return currA
+    let curr1=headA
+    let curr2=headB
+    if(lengthB>lengthA){
+        for(let i=0;i<lengthB-lengthA;i++){
+            curr2=curr2.next
         }
-        currA=currA.next
-        currB=currB.next
+        while(curr1 && curr2){
+            if(curr1 == curr2){
+            return curr1
+        }
+        else{
+            curr1=curr1.next
+            curr2=curr2.next
+        }
+        }
     }
-    return null
+    else{
+        for(let i=0;i<lengthA-lengthB;i++){
+            curr1=curr1.next
+        }
+        while(curr1 && curr2){
+        if(curr1 == curr2){
+            return curr1
+        }
+        else{
+            curr1=curr1.next
+            curr2=curr2.next
+        }
+        }
+    }
 };

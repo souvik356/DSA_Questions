6b216dd3ea53b6ad81183a1a99e35ad964004b28
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-05-10 13:14:29 +0530

    Time: 1 ms (75.03%), Space: 59.5 MB (28.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,19 +11,14 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if(!root) return []
-    let queue = [root]
-    let ans = []
-    while(queue.length){
-        let levelArr = []
-        let levelSize = queue.length
-        for(let i=0;i<levelSize;i++){
-            let curr= queue.shift()
-            curr.left && queue.push(curr.left)
-            curr.right && queue.push(curr.right)
-            levelArr.push(curr.val)
-        }
-        ans.push(levelArr)
+    let ans=[]
+    if(!root) return ans
+    function traverse(curr,level){
+        if(!ans[level]) ans[level]=[]
+        ans[level].push(curr.val)
+        curr.left && traverse(curr.left,level+1)
+        curr.right && traverse(curr.right,level+1)
     }
+    traverse(root,0)
     return ans
 };
